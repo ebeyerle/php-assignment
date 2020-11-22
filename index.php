197b70fac573778711cf8f5575f89c708662685c
Evan Beyerle <ebeyer2007@hotmail.com>
2020-11-22 14:31:08 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <meta charset="UTF-8">
+    <title>Example</title>
+    <style>
+      body
+      {
+        padding: 0px;
+        margin: 0px;
+        overflow: hidden;
+      }
+
+      .centeredContent
+      {
+          position:relative;
+          text-align:center;
+          padding-top: 25px;
+      }
+
+      .buttonGap
+      {
+          margin: 5px;
+      }
+
+    </style>
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css"/>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    
+  </head>
+  <body>
+
+  <div class="centeredContent">
+    <button type="button" class="btn buttonGap btn-primary">Primary</button>
+  </div>
+
+
+  <div class="centeredContent">
+
+    <?php
+
+      // run this test with a local server of your choice.
+      // For windows you can use: https://www.apachefriends.org/index.html
+      // for mac you can use: https://www.mamp.info/en/mamp/mac/
+
+      // key = caption, value = css class
+      $buttons = array(
+        "Success" => "btn-success",
+        "Danger" => "btn-danger",
+      );
+      // loop through the $buttons array
+      // using a custom php class you defined, output 2 buttons on the page using the defined caption and css class in $buttons
+      // the result should look like the provided image in folder.
+    ?>
+
+
+  </div>
+
+  </body>
+</html>
+
+<?php
+
+// define your php class below
+// name it "Button"
+class Button {
+	public $key;
+	public $value;
+	
+	function __construct()
+}
+
+
+?>
